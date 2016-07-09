6ec21aecdf0155d3e9c9e6e2c7beff297077dcb9
Michael P. Geraci <me@mgeraci.com>
2016-07-09 00:15:37 -0400

    ordering and some styles

--- cocktails/cocktails_app/models.py
+++ cocktails/cocktails_app/models.py
@@ -5,12 +5,18 @@ class IngredientCategory(models.Model):
     name = models.CharField(max_length=200, default="Alcohol")
     slug = models.SlugField(max_length=200, unique=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __unicode__(self):
         return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(IngredientCategory, default="Alcohol")
+
+    class Meta:
+        ordering = ['name']
 
     def __unicode__(self):
         return u'{} - {}'.format(self.category, self.name)
