8bfa9a9896a0ddedbf251c3dc7b65edbfb656cb8
Michael P. Geraci <me@mgeraci.com>
2016-07-16 11:34:33 -0400

    add template tag for formatting values

---
+++ cocktails/cocktails_app/templatetags/recipe_extras.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+import re
+
+register = template.Library()
+
+
+value_re = re.compile('\d+\.\d+')
+
+def format_value(step):
+    iterator = re.finditer(value_re, step)
+
+    for match in iterator:
+        num = match.group()
+        (whole, fraction) = num.split('.')
+
+        if whole == '0':
+            step = step.replace('0', '')
+
+        if fraction == '0':
+            step = step.replace(num, whole)
+        elif fraction == '25':
+            step = step.replace('.25', u'¼')
+        elif fraction == '33':
+            step = step.replace('.33', u'⅓')
+        elif fraction == '5':
+            step = step.replace('.5', u'½')
+        elif fraction == '66':
+            step = step.replace('.66', u'⅔')
+        elif fraction == '75':
+            step = step.replace('.75', u'¾')
+
+    return step
+
+
+register.filter('format_value', format_value)
---
+++ cocktails/cocktails_app/tests/test_recipe_extras.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+class RecipeExtrasTests(TestCase):
+    def testTest(self):
+        self.assertEqual(1, 1)
