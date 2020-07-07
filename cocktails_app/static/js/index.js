0c82ca1ff5f0a1fa2d574beba80d2f1eb2969ad6
Michael P. Geraci <me@mgeraci.com>
2020-07-07 00:37:26 -0500

    add a simple useragent check for "touchevents" for modernizr-like style changes

--- cocktails_app/static/js/index.js
+++ cocktails_app/static/js/index.js
@@ -5,10 +5,18 @@ import Login from './login';
 import MobileMenu from './mobileMenu';
 import Recipe from './recipe';
 import Search from './search';
+import { isMobile } from './util/helpers';
 
 import '../css/styles.sass';
 
 Fastclick.attach(document.body);
+
+// add a body class for some minor style changes by platform
+if (isMobile()) {
+  document.body.classList.add('mobile');
+} else {
+  document.body.classList.add('desktop');
+}
 
 Login.init();
 MobileMenu.init();
