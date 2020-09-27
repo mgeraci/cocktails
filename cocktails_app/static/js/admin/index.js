eaaf1dcbe2314ef76275de4e780efc2fb9a7516f
Michael P. Geraci <me@mgeraci.com>
2020-09-27 18:55:09 -0500

    admin: watch ingredient selects for django-added ingredients and rerun chosen

--- cocktails_app/static/js/admin/index.js
+++ cocktails_app/static/js/admin/index.js
@@ -1,15 +1,60 @@
+/**
+ * Note: changes to this file don't show up until the python server is
+ * restarted (because python reads the manifest on boot).
+ */
 import $ from 'jquery';
 
-// import chosen, disable linting. this allows $(el).chosen() to work.
+// import chosen while disabling linting; by importing it at all it allows
+// $(el).chosen() to work.
 import chosen from 'chosen-js'; // eslint-disable-line
 
 import 'chosen-js/chosen.css';
 import './index.scss';
 
-$(() => {
-  $('.form-row:not(.empty-form) .field-ingredient select').chosen();
+const SELECTS = '.form-row:not(.empty-form) .field-ingredient select';
+const REFRESH_INTERVAL = 1000;
+let ingredientCounts = [];
 
-  $('.add-row a').on('click', () => {
-    $('.form-row:not(.empty-form) .field-ingredient select').chosen();
+const bindChosen = () => {
+  $(SELECTS).chosen();
+};
+
+const getIngredientCounts = () => (
+  $(SELECTS).map((i, el) => $(el).find('option').length)
+);
+
+// loop through the selects and compare the number of ingredients to the
+// previous run's number of ingredients. if they differ, update chosen.
+const updateChosens = () => {
+  const newIngredientCounts = getIngredientCounts();
+
+  ingredientCounts.each((i, count) => {
+    if (count === newIngredientCounts[i]) {
+      return;
+    }
+
+    $(SELECTS).eq(i).trigger('chosen:updated');
   });
+
+  ingredientCounts = newIngredientCounts;
+};
+
+$(() => {
+  // short circuit on admin pages without selects
+  if (!$(SELECTS).length) {
+    return;
+  }
+
+  // bind initial dropdown
+  bindChosen();
+
+  // when we add an ingredient row, bind dropdowns again
+  $('.add-row a').on('click', () => bindChosen());
+
+  // get the initial ingredient counts
+  ingredientCounts = getIngredientCounts();
+
+  // refresh the list of chosen options on an interval to catch newly added
+  // options
+  setInterval(() => updateChosens(), REFRESH_INTERVAL);
 });
