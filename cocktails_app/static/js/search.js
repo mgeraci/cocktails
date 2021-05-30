518d7d9823e341176ec103d3a3d47dfe7b71a9d0
Michael P. Geraci <me@mgeraci.com>
2021-05-30 12:39:17 -0500

    add desktop keyboard shortcut for search

--- cocktails_app/static/js/search.js
+++ cocktails_app/static/js/search.js
@@ -2,6 +2,12 @@
 
 const Search = {
   init() {
+    this.autofocus();
+    this.addKeyboardShortcut();
+  },
+
+  // mobile only, since the search page only exists on mobile
+  autofocus() {
     const input = document.querySelectorAll('.page-content input[name=query]');
 
     if (
@@ -14,6 +20,24 @@ const Search = {
     setTimeout(() => {
       input[0].focus();
     }, 250);
+  },
+
+  addKeyboardShortcut() {
+    const input = document.querySelectorAll('.page-header input[name=query]');
+
+    if (
+      !document.querySelectorAll('.desktop').length
+      || !input.length
+    ) {
+      return;
+    }
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key === '/') {
+        e.preventDefault();
+        input[0].focus();
+      }
+    });
   },
 };
 
