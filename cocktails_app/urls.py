133b02db3b70d32f6aa289ed9588d489bac77d55
Michael P. Geraci <me@mgeraci.com>
2021-08-05 22:37:19 -0500

    django 3.1.12

--- cocktails_app/urls.py
+++ cocktails_app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -7,28 +7,28 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # admin
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 
     # main pages
-    url(r'^$', views.index, name='index_url'),
+    re_path(r'^$', views.index, name='index_url'),
 
-    url(r'^ingredients/$', views.ingredients, name='ingredients_url'),
-    url(r'^sources/$', views.sources, name='sources_url'),
-    url(r'^recipes/$', views.recipes, name='recipes_url'),
-    url(r'^search/$', views.search, name='search_url'),
+    re_path(r'^ingredients/$', views.ingredients, name='ingredients_url'),
+    re_path(r'^sources/$', views.sources, name='sources_url'),
+    re_path(r'^recipes/$', views.recipes, name='recipes_url'),
+    re_path(r'^search/$', views.search, name='search_url'),
 
     # individual pages
-    url(r'^ingredient/(?P<slug>[^/]+)/$', views.ingredient, name='ingredient_url'),
-    url(r'^source/(?P<slug>[^/]+)/$', views.source, name='source_url'),
-    url(r'^recipe/(?P<slug>[^/]+)/$', views.recipe, name='recipe_url'),
-    url(r'^recipe/l/(?P<slug>[^/]+)/$', views.recipe_share, name='recipe_share_url'),
+    re_path(r'^ingredient/(?P<slug>[^/]+)/$', views.ingredient, name='ingredient_url'),
+    re_path(r'^source/(?P<slug>[^/]+)/$', views.source, name='source_url'),
+    re_path(r'^recipe/(?P<slug>[^/]+)/$', views.recipe, name='recipe_url'),
+    re_path(r'^recipe/l/(?P<slug>[^/]+)/$', views.recipe_share, name='recipe_share_url'),
 
     # authentication
-    url(r'^api_login/$', views.api_login, name='api_login_url'),
-    url(r'^login/$', views.site_login, name='login_url'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(),
+    re_path(r'^api_login/$', views.api_login, name='api_login_url'),
+    re_path(r'^login/$', views.site_login, name='login_url'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(),
         {'next_page': '/'}, name='logout_url'),
 
     # ios app
-    url(r'^apple-app-site-association', views.apple_app_site_association),
+    re_path(r'^apple-app-site-association', views.apple_app_site_association),
 ]
